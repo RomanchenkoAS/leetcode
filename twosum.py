5d0bd020f331c44829486d81f948ca26771fd883
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-08 18:30:06 +0600

    done

--- twosum.py
+++ twosum.py
@@ -5,16 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # Faster
         for i in range(0, len(nums)):
             _tmp = target - nums[i]
             if _tmp in nums[i + 1:]:
                 return [ i , nums[i + 1:].index(_tmp) + i + 1]
 
+        # Slower 
+        # i = 0
+        # while True:
+        #     if target - nums[i] in nums[i + 1:]:
+        #         return [ i , nums[i + 1:].index(target - nums[i]) + i + 1]
+        #     i += 1
+            
+
 
 solution = Solution()
 
-nums = [2,7,11,15]
-target = 9
+nums = [3,3]
+target = 6
 
 result = solution.twoSum(nums, target)
 
