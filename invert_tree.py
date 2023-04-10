e6cc73c10ad87cbdb054322193a011cf63ccf6fc
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-10 14:22:46 +0600

    beats 96% ;)

--- invert_tree.py
+++ invert_tree.py
@@ -4,24 +4,54 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution(object):
+#     def invertTree(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: TreeNode
+#         """
+#         if root:                
+#             root.left, root.right = root.right, root.left
+            
+#             if root.left:
+#                 self.invertTree(root.left)
+#             else:
+#                 root.left = None
+                
+#             if root.right:
+#                 self.invertTree(root.right)
+#             else:
+#                 root.right = None
+
+#             return root
+
+
 class Solution(object):
     def invertTree(self, root):
         """
         :type root: TreeNode
         :rtype: TreeNode
         """
-        if root:                
-            root.left, root.right = root.right, root.left
-            
-            if root.left:
-                self.invertTree(root.left)
-            else:
-                root.left = None
-                
-            if root.right:
-                self.invertTree(root.right)
-            else:
-                root.right = None
+        def swap(node):
 
-            return root
+            if (not node.left and not node.right):
+                return node
+
+            # Swap
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                swap(node.left)
+            else:
+                node.left = None
+                
+            if node.right:
+                swap(node.right)
+            else:
+                node.right = None
+
+            return node
+
+        if root:
+            return swap(root)
         
