51a525fb8821f00bda0d5630d6e6f29a1a06eef0
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-26 14:38:13 +0600

    upd

---
+++ C++/[SOLVED] binary-search.cpp
@@ -0,0 +1,75 @@
+/*Given an array of integers nums which is sorted in ascending order, and an integer target,
+write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+
+        // Ascending order
+        if (nums.empty())
+        {
+            return -1;
+        }
+        if (nums.size() == 1) {
+            return (nums[0] == target) ? 0 : -1;
+        }
+
+        // size_t size = nums.size();
+        int begin = 0, end = nums.size();
+
+        int cursor = (begin + end) / 2;
+
+        while (begin < end - 1)
+        {
+
+
+            if (nums[cursor] == target)
+            {
+                // Found it
+                return cursor;
+            }
+            else
+            {
+                if (target > nums[cursor])
+                {
+                    begin = cursor;
+                }
+                else
+                {
+                    end = cursor;
+                }
+                cursor = (begin + end) / 2;
+            }
+
+            // if (begin)
+        }
+
+        if (nums[end] == target) {
+            return end;
+        }
+        if (nums[begin] == target) {
+            return begin;
+        }
+
+        return -1;
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+
+    vector<int> input = {-1, 0, 5};
+
+    int result = sol.search(input, 2);
+
+    cout << result << endl;
+}
