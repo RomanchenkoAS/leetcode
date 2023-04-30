37cfd055685b627829ae660e9915cf4047fbb5bd
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-04-30 18:55:55 +0600

    boilerplate

---
+++ C++/[IN PROGRESS] climbing-stairs.cpp
@@ -0,0 +1,24 @@
+/*You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. 
+In how many distinct ways can you climb to the top?*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+    }
+};
+
+int main(void) {
+
+    Solution sol;
+
+    auto result = sol.climbStairs(3);
+
+    return 0;
+}
