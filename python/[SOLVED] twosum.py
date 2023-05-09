98642cfb17d3010058e49cd61f1fad1d82286daa
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-09 19:31:21 +0600

    upd

--- python/[SOLVED] twosum.py
+++ python/[SOLVED] twosum.py
@@ -5,11 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # Hashmap
+        num_map = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in num_map:
+                return [num_map[diff], i]
+            num_map[n]=i
+        
         # Faster
-        for i in range(0, len(nums)):
-            _tmp = target - nums[i]
-            if _tmp in nums[i + 1:]:
-                return [ i , nums[i + 1:].index(_tmp) + i + 1]
+        # for i in range(0, len(nums)):
+        #     _tmp = target - nums[i]
+        #     if _tmp in nums[i + 1:]:
+        #         return [ i , nums[i + 1:].index(_tmp) + i + 1]
 
         # Slower 
         # i = 0
