6268cd4480a0330a863a202d17f3d771365f256a
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-09 22:51:07 +0600

    boilerplate

---
+++ python/[IN PROGRESS] product-of-array-except-self.py
@@ -0,0 +1,23 @@
+"""Given an integer array nums, return an array answer such that answer[i] is equal 
+to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation."""
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        
+        return None
+    
+    
+s = Solution()
+
+input = None
+
+print(s.productExceptSelf(input))
