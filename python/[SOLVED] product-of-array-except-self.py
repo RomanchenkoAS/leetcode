d329159bc629082888520fd39025cb760a450bb5
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-11 20:07:57 +0600

    upd

--- python/[SOLVED] product-of-array-except-self.py
+++ python/[SOLVED] product-of-array-except-self.py
@@ -5,31 +5,32 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
 
 You must write an algorithm that runs in O(n) time and without using the division operation."""
 
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        def product(array):
-            prod = 1
-            for i in array:
-                prod *= i
-            return prod
-        result = []
-        for i in range(len(nums)):
-            # print(i)
-            # print(nums[0:i])
-            # print(nums[i+1:])
-            result.append(product(nums[0:i])*product(nums[i + 1:]))
-            
+        n = len(nums)
+        left = [1] * n
+        right = [1] * n
         
-        return result
-    
-    
+        for i in range(1, n):
+            left[i] = left[i - 1] * nums[i - 1]
+            
+        for i in range(n-2, -1, -1):
+            right[i] = right[i + 1] * nums[i + 1]
+
+        return [right[i] * left[i] for i in range(0,n)]
+
+
 s = Solution()
 
 # input = [-1,1,0,-3,3]
-input = [1,2,3,4]
+input = [1, 2, 3, 4]
+
+print("initial:")
+print(input)
 
 print(s.productExceptSelf(input))
