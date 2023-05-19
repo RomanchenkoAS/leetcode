59db76ef472100d1a625c53c4906367d68aaea28
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-19 15:54:47 +0600

    upd

---
+++ python/[SOLVED] top-k-frequent-words.py
@@ -0,0 +1,35 @@
+"""Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the 
+same frequency by their lexicographical order."""
+
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        d = {}
+        
+        for word in words:
+            # d.setdefault(word, 0)
+            if word not in d:
+                d[word] = 1
+            else:
+                d[word] += 1
+        
+        items_list = sorted(list(d.items()), key=lambda x: (-x[1], x[0]))
+
+        return [items_list[i][0] for i in range(k)]
+
+
+s = Solution()
+
+# words = ["i", "love", "leetcode", "i", "love", "coding"]
+# words = ["the","day","is","sunny","the","the","the","sunny","is","is"]
+words = ["aaa","aa","a"]
+k = 1
+
+print(s.topKFrequent(words=words, k=k)) 
