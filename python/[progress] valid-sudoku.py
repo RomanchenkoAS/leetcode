ee577b8f528b3f3362e9f3772f1614ff52896238
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-19 21:14:44 +0600

    upd

--- python/[progress] valid-sudoku.py
+++ python/[progress] valid-sudoku.py
@@ -8,27 +8,51 @@ Note:
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.'''
 
+
 class Solution(object):
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
         """
-        
+
         # Get 3x3 pieces
-        
-        
-        
+
+        row = set()
+        # cols = [set() for _ in range(9)]
+        cols = [set()] * 9
+        boxes = [set()] * 9
+
+        print(cols)
+        # print(boxes)
+
+        for li, line in enumerate(board):
+
+            # Reset list for row
+            row.clear()
+            for ci, cell in enumerate(board[li]):
+                # print(cell)
+
+                # Row
+                if cell in row:
+                    return False
+                else:
+                    row.add(cell)
+                    
+                # Column 
+                if cell in cols[ci]:
+                    return False
+                else:
+                    cols[ci].add(cell)
+                    
+                # Box
+
+            # After row is processed
+
+
 s = Solution()
 
-board = [["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
+board = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8",
+                                                                                                                                                                                                      ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
 
 print(s.isValidSudoku(board))
