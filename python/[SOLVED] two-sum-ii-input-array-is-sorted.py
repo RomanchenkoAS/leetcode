2474664c7ec5b67d1c76eb9211c802991dde1009
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-20 23:15:36 +0600

    upd

---
+++ python/[SOLVED] two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,28 @@
+"""Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+find two numbers such that they add up to a specific target number. Let these two numbers be 
+numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space."""
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        l = 0
+        r = len(numbers) - 1
+        
+        while l <= r:
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                return [l + 1, r + 1]
