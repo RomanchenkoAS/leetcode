79836c7aa2db12f7b4a2c3da30aba777a1594692
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-05-26 22:31:23 +0600

    uod

--- python/[progress] 3sum.py
+++ python/[progress] 3sum.py
@@ -3,47 +3,49 @@
 
 Notice that the solution set must not contain duplicate triplets."""
 
+
 class Solution(object):
     def threeSum(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+
         result = []
-        
-        nums = sorted(nums)
-        
+
+        nums.sort()
+
         for i, num in enumerate(nums):
             target = -num
             l = i + 1
             r = len(nums) - 1
-            
+
             if i > 0 and nums[i - 1] == nums[i]:
                 continue
-             
+
             while l < r:
-                
-                if l > i + 1 and nums[l] == nums[l - 1]:
-                    l += 1
-                    continue
-                if r < len(nums) - 1 and nums[r] == nums[r + 1]:
-                    r -= 1
-                    continue
-                
+
                 if target == nums[l] + nums[r]:
                     result.append([num, nums[l], nums[r]])
                     l += 1
                     r -= 1
                 elif target < nums[l] + nums[r]:
                     r -= 1
+
+                    while nums[r] == nums[r + 1] and l < r:
+                        r -= 1
                 else:
                     l += 1
-        
+
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
         return result
-    
+
+
 s = Solution()
 # nums = [-1,0,1,2,-1,-4]
-nums = [0, 0, 0, 0]
+# nums = [0, 0, 0, 0]
+nums = [-2, 0, 0, 2, 2]
 
-print(s.threeSum(nums=nums)) 
+print(s.threeSum(nums=nums))
