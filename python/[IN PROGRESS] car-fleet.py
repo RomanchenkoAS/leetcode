979cb1e1b9dbbc72d9600c31c2765d8da5f281e9
Romanchenko Artur <RomanchenkoAS@yandex.ru>
2023-10-30 22:15:33 +0600

    boilerplate

---
+++ python/[IN PROGRESS] car-fleet.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pass
+
+
+s = Solution()
+target = 12
+position = [10, 8, 0, 5, 3]
+speed = [2, 4, 1, 1, 3]
+print(s.carFleet(target, position, speed))
