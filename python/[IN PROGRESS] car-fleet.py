d2b5288337290b16e6146ffe740c3b7713b86ae2
Romanchenko Artur <RomanchenkoAS@yandex.ru>
2023-10-30 22:59:43 +0600

    done

--- python/[IN PROGRESS] car-fleet.py
+++ python/[IN PROGRESS] car-fleet.py
@@ -1,6 +1,19 @@
+from typing import List
+
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pass
+        #        Time each car needs to reach the target (sorted by position)
+        pos_speed = [(pos, speed) for pos, speed in zip(position, speed)]
+        pos_speed.sort(reverse=True, key=lambda x: x[0])
+        stack = []
+
+        for pos, speed in pos_speed:
+            time = (target - pos) / speed
+            if stack and stack[-1] >= time:
+                time = stack.pop()
+            stack.append(time)
+        return len(stack)
 
 
 s = Solution()
