75701345b72e55dcc1e7d14774eb768053d6895a
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-03-02 18:30:05 +0300

    done

--- python/task.py
+++ python/task.py
@@ -10,8 +10,10 @@ import json
 input_list = [{"key1": "value1"}, {"k1": "v1", "k2": "v2", "k3": "v3"}, {}, {}, {"key1": "value1"}, {"key1": "value1"},
               {"key2": "value2"}]
 
+
 def remove_duplicates(input_list: list[dict]) -> list[dict]:
     seen = {json.dumps(d, sort_keys=True) for d in input_list}
     return [json.loads(d) for d in seen]
+
 
 print(remove_duplicates(input_list))
