469c27b558b39fe469e6b3e250b5fc82dfd4a3d2
Artur <romanchenko.artur@krasnoe-beloe.ru>
2024-08-19 09:45:33 +0300

    +8

--- python/solved/[SOLVED] product-of-array-except-self.py
+++ python/solved/[SOLVED] product-of-array-except-self.py
@@ -1,36 +1,17 @@
-"""Given an integer array nums, return an array answer such that answer[i] is equal 
-to the product of all the elements of nums except nums[i].
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        length = len(nums)
+        answers = [1] * length
+        for i in range(1, length):
+            answers[i] = answers[i - 1] * nums[i - 1]
 
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
-You must write an algorithm that runs in O(n) time and without using the division operation."""
-
-
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = len(nums)
-        left = [1] * n
-        right = [1] * n
-        
-        for i in range(1, n):
-            left[i] = left[i - 1] * nums[i - 1]
-            
-        for i in range(n-2, -1, -1):
-            right[i] = right[i + 1] * nums[i + 1]
-
-        return [right[i] * left[i] for i in range(0,n)]
+        running_sum = nums[length - 1]
+        for i in range(length - 2, -1, -1):
+            answers[i] *= running_sum
+            running_sum *= nums[i]
+        return answers
 
 
 s = Solution()
-
-# input = [-1,1,0,-3,3]
-input = [1, 2, 3, 4]
-
-print("initial:")
-print(input)
-
-print(s.productExceptSelf(input))
+nums = [1, 2, 3, 4]
+print(s.productExceptSelf(nums))
